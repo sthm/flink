61ff9d4338d5079e02aa033bda8c2d4aafc4e666
Steffen Hausmann <shausma@amazon.de>
2021-06-02 14:37:32 +0200

    Switch to ConcurrentLinkedQueue and leverage (yet to be created) functionality to signal backpressure

--- flink-connectors/flink-connector-base/src/main/java/com/amazonaws/samples/flink/api/sink/writer/AsyncSinkWriter.java
+++ flink-connectors/flink-connector-base/src/main/java/com/amazonaws/samples/flink/api/sink/writer/AsyncSinkWriter.java
@@ -1,85 +1,82 @@
 package com.amazonaws.samples.flink.api.sink.writer;
 
 import org.apache.flink.api.connector.sink.SinkWriter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.BlockingDeque;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.function.Function;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 import static org.apache.flink.util.Preconditions.checkArgument;
 
 
-public abstract class AsyncSinkWriter<InputT, RequestT extends Serializable> implements SinkWriter<InputT, Collection<CompletableFuture<?>>, Collection<RequestT>> {
+public abstract class AsyncSinkWriter<InputT, RequestEntryT extends Serializable> implements SinkWriter<InputT, Collection<CompletableFuture<?>>, Collection<RequestEntryT>> {
 
     static final Logger logger = LogManager.getLogger(AsyncSinkWriter.class);
 
     /**
      * This function specifies the mapping between elements of a stream to
-     * requests that can be sent to the API. The mapping is provided by the
-     * end-user of a sink, not the sink creator.
+     * request entries that can be sent to the API. The mapping is provided by
+     * the end-user of a sink, not the sink creator.
      * <p>
-     * The requests contain all relevant information for the request to be
-     * persisted in the destination. Eg, for Kinesis Data Streams, the request
+     * The request entries contain all relevant information required to create
+     * and sent the actual request. Eg, for Kinesis Data Streams, the request
      * contains the payload and the partition key. The requests are buffered by
      * the AipWriter and sent to the API when the {@code submitRequestsToApi}
      * method is invoked.
      */
-    private final Function<InputT, RequestT> elementToRequest;
+    private final Function<InputT, RequestEntryT> elementToRequest;
 
 
     /**
-     * This method specifies how to persist buffered requests into the sink. It
-     * is implemented when a new API endpoint should be supported.
+     * This method specifies how to persist buffered request entries into the
+     * sink. It is implemented when a new endpoint or service should be
+     * supported.
      * <p>
-     * The method is invoked with a set of requests according to the buffering
-     * hints. The logic then needs to create and execute the put against the API
-     * endpoint (ideally by batching together individual requests to increase
-     * efficiency). The logic also needs to identify individual requests that
-     * were not persisted successfully and resubmit them using the {@code
-     * requeueFailedRequest} method.
+     * The method is invoked with a set of request entries according to the
+     * buffering hints. The logic then needs to create and execute the request
+     * against the API endpoint (ideally by batching together multiple request
+     * entries to increase efficiency). The logic also needs to identify
+     * individual request entries that were not persisted successfully and
+     * resubmit them using the {@code requeueFailedRequest} method.
      * <p>
      * The method returns a future that indicates, once completed, that all
-     * requests that are passed to the method have either successfully completed
-     * or the requests have been re-queued.
+     * request entries that have been passed to the method on invocation have
+     * either successfully completed or the request entries have been
+     * re-queued.
      * <p>
      * During checkpointing, the sink needs to ensure that there are no
      * outstanding in-flight requests. Ie, that all futures returned by this
      * method are completed.
      *
-     * @param requests a set of requests that should be sent to the API
-     *                 endpoint
-     * @return a future that completes when all requests have been successfully
-     * put to the API or were requeued
+     * @param requestEntries a set of requests that should be sent to the API
+     *                       endpoint
+     * @return a future that completes when all request entries have been
+     * successfully persisted to the API or were re-queued
      */
-    protected abstract CompletableFuture<?> submitRequestsToApi(List<RequestT> requests);
+    protected abstract CompletableFuture<?> submitRequestEntries(List<RequestEntryT> requestEntries);
 
 
     /**
-     * Buffer to hold requests that should be persisted into the respective API
-     * endpoint. Using a blocking deque so that the sink can properly build
-     * backpressure.
+     * Buffer to hold request entries that should be persisted into the
+     * respective endpoint.
      * <p>
-     * A request contains all relevant details to make a request to the
-     * respective API. Eg, for Kinesis a request contains the payload and
+     * A request entry contain all relevant details to make a request to the
+     * respective API. Eg, for Kinesis a request entry contains the payload and
      * partition key.
      * <p>
      * It seems more natural to buffer InputT, ie, the events that should be
-     * persisted, rather than RequestT. However, in practice, the response of a
-     * failed API request call can make it very hard, if not impossible, to
+     * persisted, rather than RequestEntryT. However, in practice, the response
+     * of a failed request call can make it very hard, if not impossible, to
      * reconstruct the original event. It is much easier, to just construct a
-     * new (retry) request from the response and add that back to the queue for
-     * later retry.
+     * new (retry) request entry from the response and add that back to the
+     * queue for later retry.
      */
-    private transient final BlockingDeque<RequestT> bufferedRequests = new LinkedBlockingDeque<>();
+    private final Queue<RequestEntryT> bufferedRequests = new ConcurrentLinkedQueue<>();
 
 
     /**
@@ -94,32 +91,22 @@ public abstract class AsyncSinkWriter<InputT, RequestT extends Serializable> imp
      * To complete a checkpoint, we need to make sure that no requests are in
      * flight, as they may fail which could then lead to data loss.
      */
-    private BlockingDeque<CompletableFuture<?>> inFlightRequests = new LinkedBlockingDeque<>();
+    private Queue<CompletableFuture<?>> inFlightRequests = new ConcurrentLinkedQueue<>();
 
 
 
     @Override
     public void write(InputT element, Context context) throws IOException {
-        try {
-            bufferedRequests.putLast(elementToRequest.apply(element));
+        bufferedRequests.add(elementToRequest.apply(element));
 
-            flush();  // just for testing
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            //TODO: handle exception; logger warn
-        }
+        flush();  // just for testing
     }
 
     /**
-     * Put a failed request back into the internal queue to retry later.
+     * Put a failed request entry back into the internal queue for later retry.
      */
-    protected void requeueFailedRequest(RequestT request) {
-        try {
-            bufferedRequests.putFirst(request);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            //TODO: handle exception; logger warn
-        }
+    protected void requeueFailedRequestEntry(RequestEntryT requestEntry) {
+        bufferedRequests.add(requestEntry);
     }
 
 
@@ -127,7 +114,7 @@ public abstract class AsyncSinkWriter<InputT, RequestT extends Serializable> imp
 
     private static final int BATCH_SIZE = 100;       // just for testing purposes
 
-    public AsyncSinkWriter(Function<InputT, RequestT> elementToRequest) {
+    public AsyncSinkWriter(Function<InputT, RequestEntryT> elementToRequest) {
         this.elementToRequest = elementToRequest;
     }
 
@@ -139,28 +126,28 @@ public abstract class AsyncSinkWriter<InputT, RequestT extends Serializable> imp
      */
     public void flush() {
         while (bufferedRequests.size() >= BATCH_SIZE) {
-            ArrayList<RequestT> batch = new ArrayList<>();
+            ArrayList<RequestEntryT> batch = new ArrayList<>();
 
             for (int i=0; i<BATCH_SIZE; i++) {
-                RequestT request = bufferedRequests.remove();
+                RequestEntryT request = bufferedRequests.remove();
                 batch.add(request);
             }
 
             logger.info("submit requests for {} elements", batch.size());
 
-            inFlightRequests.add(submitRequestsToApi(batch));
+            inFlightRequests.add(submitRequestEntries(batch));
         }
     }
 
 
     /**
-     * In flight requests may fail, they will be retried if the sink is still
-     * healthy.
+     * In flight requests may fail, but they will be retried if the sink is
+     * still healthy.
      * <p>
      * To not lose any requests, there cannot be any outstanding in-flight
      * requests when a checkpoint/commit is in process. To this end, all
      * in-flight requests need to be completed and no new requests can be
-     * created during a checkpoint.
+     * created until a checkpoint/savepoint completed.
      */
     @Override
     public List<Collection<CompletableFuture<?>>> prepareCommit(boolean flush) throws IOException {
@@ -174,17 +161,21 @@ public abstract class AsyncSinkWriter<InputT, RequestT extends Serializable> imp
 
         // reuse current inFlightRequests as commitable and create empty queue to avoid copy and clearing
         List<Collection<CompletableFuture<?>>> committable = Collections.singletonList(inFlightRequests);
-        inFlightRequests = new LinkedBlockingDeque<>();
+
+        // during a checkpoint/savepoint, no new requests can be created, so it's save to create a new queue
+        inFlightRequests = new ConcurrentLinkedQueue<>();
 
         return committable;
     }
 
     /**
-     * Buffered but not yet submitted requests are stored in the
-     * checkpoint/savepoint.
+     * All in-flight requests have been completed, but there may still be
+     * request entries in the internal buffer that are yet to be sent to the
+     * endpoint. These request entries are stored in the snapshot state so that
+     * they don't get lost in case of a failure/restart of the application.
      */
     @Override
-    public List<Collection<RequestT>> snapshotState() throws IOException {
+    public List<Collection<RequestEntryT>> snapshotState() throws IOException {
         checkArgument(inFlightRequests.isEmpty());
 
         //TODO: enable submission of new api calls once checkpoint has been completed
